36371bf06674fef31c8915d24d2654916120b0b4
feherdaniel <feherdaniel2010@gmail.com>
2021-08-10 11:25:41 +0200

    feat: EmailService class Send method

---
+++ SaintSender.Core/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+using SaintSender.Core.Models;
+using System.Net;
+
+namespace SaintSender.Core.Services
+{
+    public class EmailService
+    {
+        public void Send(string to, Credentials from, string subject, string content)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(from.Name, from.EmailAddress));
+            message.To.Add(new MailboxAddress("", to));
+            message.Subject = subject;
+            message.Body = new TextPart("plain")
+            {
+                Text = content
+            };
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate(from.EmailAddress, from.Password);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
