99e82134c0877e4fc1e8ec5946c4da2c8445c445
feherdaniel <feherdaniel2010@gmail.com>
2021-08-10 11:24:56 +0200

    feat: Added Name property

--- SaintSender.Core/Models/Credentials.cs
+++ SaintSender.Core/Models/Credentials.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace SaintSender.Core.Models
 {
-    class Credentials
+    public class Credentials
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public string Name { get; set; }
 
-        public Credentials(string emailAddress, string password)
+        public Credentials(string emailAddress, string password, string name)
         {
             EmailAddress = emailAddress;
             Password = password;
+            Name = name;
         }
     }
 }
