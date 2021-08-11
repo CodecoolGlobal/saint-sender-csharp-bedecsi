1a4f7917bebf3a1421ae503dc7bdf19018590de0
feherdaniel <feherdaniel2010@gmail.com>
2021-08-11 11:30:28 +0200

    feat: implement cancel button click handler

--- SaintSender.DesktopUI/Views/NewEmail.xaml.cs
+++ SaintSender.DesktopUI/Views/NewEmail.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using SaintSender.Core.Models;
 using SaintSender.DesktopUI.ViewModels;
 
@@ -21,8 +8,8 @@ namespace SaintSender.DesktopUI.Views
     /// Interaction logic for NewEmail.xaml
     /// </summary>
     /// 
-    
-    public partial class NewEmail : Page
+
+    public partial class NewEmail : Window
     {
         public Credentials Credentials { get; set; }
 
@@ -42,7 +29,7 @@ namespace SaintSender.DesktopUI.Views
 
         private void CancelBtn_Click(object sender, RoutedEventArgs e)
         {
-            // not exactly sure what to do here
+            Close();
         }
     }
 }
