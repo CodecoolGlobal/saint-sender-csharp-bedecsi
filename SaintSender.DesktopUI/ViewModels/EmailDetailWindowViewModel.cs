5c4e700874a97d2613e07533ffd24eb97a09c930
feherdaniel <feherdaniel2010@gmail.com>
2021-08-12 14:32:02 +0200

    refactor: change location of ViewModel

--- SaintSender.DesktopUI/ViewModels/EmailDetailWindowViewModel.cs
+++ SaintSender.DesktopUI/ViewModels/EmailDetailWindowViewModel.cs
@@ -1,10 +1,5 @@
-﻿using SaintSender.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using SaintSender.Core.Models;
 
 namespace SaintSender.DesktopUI.Views
 {
